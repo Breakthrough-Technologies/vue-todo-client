3ad39ce7a45a2c1945ab0ad4336993a11bb7b91a
breadthe <constantin.cojocaru@breaktech.com>
2017-10-08 21:04:09 -0500

    Add Bulma and improve layout

--- src/App.vue
+++ src/App.vue
@@ -10,13 +10,17 @@ export default {
 }
 </script>
 
+<style lang="sass" src="bulma"></style>
+<style lang="sass">
+  $fa-font-path: '../node_modules/font-awesome/fonts/';
+  @import '../node_modules/font-awesome/scss/font-awesome';
+</style>
+
 <style>
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,30 +1,62 @@
 <template>
-  <div class="hello">
-    <h1>{{ title }}</h1>
+  <section>
 
-    <label>Add a new task <input v-model="newTask" /><button v-on:click="addTask()">Add</button></label>
+    <section class="hero is-primary">
+      <div class="hero-body">
+        <div class="container is-fullwidth">
+          <h1 class="title">
+            {{ title }}
+          </h1>
+          <h2 class="subtitle">
+            A Vue.js + Bulma app
+          </h2>
+        </div>
+      </div>
+    </section>
+  
+    <section class="section">
 
-    <h2>Tasks</h2>
-    <div v-if="tasks.length">
-      <p v-if="incompleteTasks.length">
-        <ul v-for="task in incompleteTasks">
-          <li><input type="checkbox" v-model="task.completed"> {{ task.description }}</li>
-        </ul>
-      </p>
-      <p v-else>
-        All tasks have been completed!
-      </p>
-      <hr>
-      <p v-if="completedTasks.length">
-        <ul v-for="task in completedTasks">
-          <li><input type="checkbox" v-model="task.completed"> {{ task.description }}</li>
-        </ul>
-      </p>
-    </div>
-    <div v-else>
-      There are no tasks yet
-    </div>
-  </div>
+      <nav class="panel">
+        <div class="panel-heading">
+          Tasks
+        </div>
+        <div class="panel-block">
+          <div class="field has-addons">
+            <div class="control">
+              <input class="input" type="text" placeholder="Add a new task" v-model="newTask" />
+            </div>
+            <div class="control">
+              <button class="button is-primary" v-on:click="addTask()"><i class="fa fa-plus" aria-hidden="true"></i></button>
+            </div>
+          </div>
+        </div>
+        <div class="panel-tabs">
+          <a class="is-active">all</a>
+          <a>completed</a>
+        </div>
+        <div v-if="tasks.length">
+          <div v-if="incompleteTasks.length">
+            <span class="panel-block" v-for="task in incompleteTasks">
+              <input type="checkbox" v-model="task.completed"> {{ task.description }}
+            </span>
+          </div>
+          <div v-if="completedTasks.length">
+            <span class="panel-block"><strong>Completed tasks</strong></span>
+            <span class="panel-block" v-for="task in completedTasks">
+              <input type="checkbox" v-model="task.completed"> {{ task.description }}
+            </span>
+          </div>
+        </div>
+        <div v-else>
+          <span class="panel-block">
+            There are no tasks yet
+          </span>
+        </div>
+      </nav>
+
+    </section>
+
+  </section>
 </template>
 
 <script>
