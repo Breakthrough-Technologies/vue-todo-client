f102b35f19e523bb52a6a971fcf26fc742263dee
breadthe <constantin.cojocaru@breaktech.com>
2017-10-14 11:57:38 -0500

    Improve how new tasks are added

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -23,10 +23,10 @@
         <div class="panel-block">
           <div class="field has-addons">
             <div class="control">
-              <input class="input" type="text" placeholder="Add a new task" v-model="newTask" />
+              <input class="input" type="text" placeholder="Add a new task" v-model="newTask" v-on:keyup="whileTyping" />
             </div>
             <div class="control">
-              <button class="button is-primary" v-on:click="addTask()"><i class="fa fa-plus" aria-hidden="true"></i></button>
+              <button class="button is-primary" v-on:click="addTask" v-html="buttons.addTask.icon" v-bind:title="buttons.addTask.title" v-bind:disabled="buttons.addTask.disabled"></button>
             </div>
           </div>
         </div>
@@ -66,13 +66,33 @@ export default {
     return {
       title: 'ToDo List',
       newTask: '',
-      tasks: []
+      tasks: [],
+      buttons: {
+        addTask: {
+          title: 'Click to add a new task',
+          icon: `<i class="fa fa-plus" aria-hidden="true"></i>`,
+          disabled: true
+        }
+      }
     }
   },
   methods: {
+    whileTyping: function (e) { // when the user types in the text box...
+      if (this.newTask.length) {
+        this.buttons.addTask.disabled = false // enable the button since we have some text in the box
+        if (e.key === 'Enter') { // if the Enter key was pressed
+          this.addTask() // add the task
+        }
+      } else { // disable the button if there's no text
+        this.buttons.addTask.disabled = true
+      }
+    },
     addTask: function () {
-      this.tasks.push({description: this.newTask, completed: false})
-      this.newTask = ''
+      if (this.newTask.length) {
+        this.tasks.push({description: this.newTask, completed: false})
+        this.newTask = '' // clear the text box
+        this.buttons.addTask.disabled = true // once we're done adding the task, disable the button again
+      }
     }
   },
   computed: {
